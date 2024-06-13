755fe5d5c8e421bd7c58553f0943ca3a0d914341
andriellibello <andriellibello86@gmail.com>
2024-06-13 15:51:53 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 import {PencilLine}from '@phosphor-icons/react'
 
@@ -10,8 +11,8 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/162721865?v=4"/>
-
+        <Avatar src="https://avatars.githubusercontent.com/u/162721865?v=4"/>
+        
         <strong>Andrielli Bello</strong>
         <span>Developer</span>
       </div>
